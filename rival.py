f76c491f875fedd03164a05a85d1f5017428a0c4
Demon000 <demonsingur@gmail.com>
2019-02-18 15:46:56 +0200

    rivalcolor: move to command line arguments

--- rival.py
+++ rival.py
@@ -2,20 +2,46 @@
 
 import time
 from itertools import cycle
+from optparse import OptionParser
 
 import rivalcfg
 
-UPDATE_TIME = 0.05
-WAIT_TIME = 1
+from rivalcfg.helpers import is_color, color_string_to_rgb
 
-config = [
-  (5, (0, 0, 255)),
-  (5, (0, 255, 255)),
-  (5, (0, 255, 0)),
-  (5, (255, 255, 0)),
-  (5, (255, 255, 255)),
-  (5, (255, 0, 255)),
-]
+DEFAULT_UPDATE_TIME = 0.05
+DEFAULT_WAIT_TIME = 1
+
+parser = OptionParser()
+parser.add_option('-u', '--update-time', dest='update_time', default=DEFAULT_UPDATE_TIME, type=float)
+parser.add_option('-w', '--wait-time', dest='wait_time', default=DEFAULT_WAIT_TIME, type=float)
+
+(options, args) = parser.parse_args()
+
+values = vars(options)
+
+update_time = values['update_time']
+wait_time = values['wait_time']
+
+config = []
+for arg in args:
+  duration_string, color_string = arg.split(':')
+
+  try:
+    duration = int(duration_string)
+  except ValueError:
+    print('`{}` is not a valid duration.', )
+    exit()
+
+  if not is_color(color_string):
+    print('`{}` is not a valid color.'.format(color_string))
+    exit()
+
+  color = color_string_to_rgb(color_string)
+  config.append((duration, color))
+
+if not config:
+  print('No colors have been defined.')
+  exit()
 
 def t_mul(t, s):
   return tuple(x * s for x in t)
@@ -42,7 +68,7 @@ def wait_for_mouse():
       print("Found: {}".format(mouse))
       return mouse
 
-    time.sleep(WAIT_TIME)
+    time.sleep(wait_time)
 
 mouse = wait_for_mouse()
 
@@ -58,7 +84,7 @@ for (color_time, color) in cycle(config):
 
     mouse.save()
 
-    current_color_time += UPDATE_TIME
-    time.sleep(UPDATE_TIME)
+    current_color_time += update_time
+    time.sleep(update_time)
 
   last_color = color
