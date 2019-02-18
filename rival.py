59974201c2484be1971c2346808cabb598a08e87
Demon000 <demonsingur@gmail.com>
2019-02-18 12:58:43 +0200

    rivalcolor: update for new rivalcfg api

--- rival.py
+++ rival.py
@@ -3,8 +3,7 @@
 import time
 from itertools import cycle
 
-import rivalcfg.rival_mouse as mouse
-import rivalcfg.cli as cli
+import rivalcfg
 
 delta = 0.05
 
@@ -23,19 +22,18 @@ def interpolate(t, a, b):
   return tuple( int((1 - t) * x + t * y) for (x, y) in zip(a, b))
 
 while True:
-  profile = cli.get_plugged_mouse_profile()
-  if profile is None:
+  mouse = rivalcfg.get_first_mouse()
+  if mouse is None:
     time.sleep(1)
   else:
-    print("Found mouse: {}".format(profile["name"]))
+    print("Found mouse: {}".format(mouse))
     try:
-      instance = mouse.RivalMouse(profile)
       prev = (0, 0, 0)
       for (total_time, color) in cycle(config):
         t = 0
         while t < total_time:
           c = interpolate(t / total_time, prev, color)
-          instance.set_color(*c)
+          mouse.set_color(*c)
           t += delta
           time.sleep(delta)
         prev = color
