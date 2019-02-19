e07d6364ed451164de8c8a865a6a45fc3642cbba
Demon000 <demonsingur@gmail.com>
2019-02-19 13:10:58 +0200

    rivalcolor: remove one time constants

--- rival.py
+++ rival.py
@@ -7,15 +7,12 @@ from helpers import text_to_morse, morse_to_config, args_to_config, color_from_s
 from iterator_color import IteratorColor
 from sound_color import SoundColor
 
-DEFAULT_UPDATE_TIME = 0.05
-DEFAULT_WAIT_TIME = 1
-
 description = 'Colors: a list of colors to cycle through, ' + \
     'for example: 1:#ff0000 0.5:ff0000 2:#f00 0.5:f00 3:red'
 usage = '%prog [options] [colors]'
 parser = OptionParser(usage=usage, description=description)
-parser.add_option('-u', '--update-time', dest='update_time', default=DEFAULT_UPDATE_TIME, type=float)
-parser.add_option('-w', '--wait-time', dest='wait_time', default=DEFAULT_WAIT_TIME, type=float)
+parser.add_option('-u', '--update-time', dest='update_time', default=0.05, type=float)
+parser.add_option('-w', '--wait-time', dest='wait_time', default=1, type=float)
 parser.add_option('-m', '--morse', action="store_true", dest='is_morse')
 parser.add_option('-s', '--sound', action="store_true", dest='is_sound')
 parser.add_option('-L', '--low', dest='low_color_string', default='#00ff00', type=str)
