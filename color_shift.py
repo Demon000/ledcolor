9e16c1e540e305e81519d758fdb0b9e4fa22ec8f
Demon000 <demonsingur@gmail.com>
2019-02-19 05:50:45 +0200

    rivalcolors: split into easier to override pieces

--- color_shift.py
+++ color_shift.py
@@ -22,21 +22,27 @@ class ColorShift():
   def __set_color(self, color):
     result = self.__mouse.set_color(*color)
     if result < 0:
-      self.__mouse = wait_for_mouse()
+      self.__mouse = self.__wait_for_mouse()
 
     self.__mouse.save()
 
+  def __get_mixed_color(self, color, color_weight):
+    return t_add_w(self.__prev_color, color, color_weight)
+
+  def __play_color(self, color_time, color):
+    current_color_time = 0
+    while current_color_time < color_time:
+      color_weight = current_color_time / color_time
+      mixed_color = self.__get_mixed_color(color, color_weight)
+
+      self.__set_color(mixed_color)
+
+      current_color_time += self.__update_time
+      time.sleep(self.__update_time)
+
+    self.__prev_color = color
+
   def run(self):
-    _, last_color = next(self.__iterator)
+    _, self.__prev_color = next(self.__iterator)
     for color_time, color in self.__iterator:
-      current_color_time = 0
-      while current_color_time < color_time:
-        color_weight = current_color_time / color_time
-        interpolated_color = t_add_w(last_color, color, color_weight)
-
-        self.__set_color(interpolated_color)
-
-        current_color_time += self.__update_time
-        time.sleep(self.__update_time)
-
-      last_color = color
+      self.__play_color(color_time, color)
