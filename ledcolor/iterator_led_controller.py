390ac1d6d3ed5104f97712a0a8992666fe1e14b8
Demon000 <demonsingur@gmail.com>
2019-03-08 09:13:51 +0200

    ledcolor: implement iterator led as a controller

--- ledcolor/iterator_led_controller.py
+++ ledcolor/iterator_led_controller.py
@@ -1,20 +1,22 @@
 from threading import Thread
 
-from led import Led
-
-class IteratorLed(Led):
-  def __init__(self, config):
-    super().__init__(config)
-
+class IteratorLedController():
+  def __init__(self, leds, config):
+    self.__leds = leds
     self.__iterator = config.iterator
+    self.__update_time = config.update_time
+
     self.__thread = Thread(target=self.__animation_work, daemon=True)
 
   def __animation_work(self):
     color = next(self.__iterator)
-    while not self._stopped:
+    while not self.__stopped:
       next_color = next(self.__iterator)
-      self._do_animated_color(color, next_color)
+      self.__leds.for_each_led('do_animated_color', color, next_color)
       color = next_color
 
   def start(self):
     self.__thread.start()
+
+  def stop(self):
+    self.__stopped = True
