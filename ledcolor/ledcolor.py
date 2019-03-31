7466aa17292cda378b26861f9cf10f75f30e2b50
Demon000 <demonsingur@gmail.com>
2019-03-31 21:33:42 +0300

    ledcolor: reorder options

--- ledcolor/ledcolor.py
+++ ledcolor/ledcolor.py
@@ -38,11 +38,11 @@ def main():
   parser.add_option('-n', '--name', dest='name', type=str)
 
   parser.add_option('-c', '--colors', action='store_true', dest='is_colors')
-  parser.add_option('-s', '--sound', action='store_true', dest='is_sound')
 
+  parser.add_option('-s', '--sound', action='store_true', dest='is_sound')
+  parser.add_option('-i', '--input', dest='input_name', type=str)
   parser.add_option('-L', '--low', dest='low_color_string', default=default_low_color, type=str)
   parser.add_option('-H', '--high', dest='high_color_string', default=default_high_color, type=str)
-  parser.add_option('-i', '--input', dest='input_name', type=str)
 
   options, args = parser.parse_args()
   config = Config(options, args)
