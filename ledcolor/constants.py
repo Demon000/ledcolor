911af293c4a61d5dea484a5cd2ccb99a367f9401
Demon000 <demonsingur@gmail.com>
2019-03-31 21:25:05 +0300

    ledcolor: lower max volume fall

--- ledcolor/constants.py
+++ ledcolor/constants.py
@@ -4,5 +4,5 @@ audio_rate = 48000
 default_update_time = 0.05
 default_low_color = '#00ff00'
 default_high_color = '#ff0000'
-max_volume_fall_time = 10
+max_volume_fall_time = 2
 max_volume_floor = 0.1
