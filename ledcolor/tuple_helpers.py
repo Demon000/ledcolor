a7ffa35051c4a1c0b2c8152ed1e6b4984e6d7ac5
Demon000 <demonsingur@gmail.com>
2020-06-14 23:09:42 +0300

    ledcolor: reformat

--- ledcolor/tuple_helpers.py
+++ ledcolor/tuple_helpers.py
@@ -1,17 +1,20 @@
 def t_mul(t, s):
-  return tuple(x * s for x in t)
+    return tuple(x * s for x in t)
+
 
 def t_add(a, b):
-  return tuple(x + y for (x, y) in zip(a, b))
+    return tuple(x + y for (x, y) in zip(a, b))
+
 
 def t_int(t):
-  return tuple(int(x) for x in t) 
+    return tuple(int(x) for x in t)
+
 
 def t_add_w(a, b, w):
-  if w > 1:
-    w = 1
+    if w > 1:
+        w = 1
 
-  if w < 0:
-    w = 0
+    if w < 0:
+        w = 0
 
-  return t_int(t_add(t_mul(a, 1 - w), t_mul(b, w)))
+    return t_int(t_add(t_mul(a, 1 - w), t_mul(b, w)))
