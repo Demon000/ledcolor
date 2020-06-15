0dbe9cbefa7121cb5803547673141c16fcecba0a
Demon000 <demonsingur@gmail.com>
2020-06-15 13:10:30 +0300

    ledcolor: make controller config protected

--- ledcolor/led_controller.py
+++ ledcolor/led_controller.py
@@ -1,10 +1,10 @@
 class LedController:
     def __init__(self, config):
         self._leds = []
-        self.__config = config
+        self._config = config
 
     def is_compatible_config(self, config):
-        return self.__config == config
+        return self._config == config
 
     def controls_led(self, led):
         return led in self._leds
