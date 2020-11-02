79d565f8e18baf2a21e5b99b0c068469811423b1
Demon000 <demonsingur@gmail.com>
2020-11-02 20:07:29 +0200

    ledcolor: fix color iteration one more time

--- controllers/iterator_led_controller.py
+++ controllers/iterator_led_controller.py
@@ -6,15 +6,20 @@ class IteratorLedController(ThreadLedController):
         super().__init__(self.__work, config)
 
         self.__colors = config.colors
+        self.__current_colors = []
         self.__update_time = config.update_time
 
-    def __work(self):
-        colors = []
-        while not self._should_stop_thread:
-            if not colors:
-                colors = self.__colors[::-1]
+    def __get_next_color(self):
+        if not self.__current_colors:
+            self.__current_colors = self.__colors[::]
 
-            next_color = colors.pop()
+        return self.__current_colors.pop()
+
+    def __work(self):
+        color = self.__get_next_color()
+        while not self._should_stop_thread:
+            next_color = self.__get_next_color()
             for led in self._leds:
+                print("setting led {}".format(led.name))
                 led.do_animated_color(color, next_color, self.__update_time)
             color = next_color
