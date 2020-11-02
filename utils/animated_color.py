9f8eba45a77b1a74cda6525b4fb8adf633ae6491
Demon000 <demonsingur@gmail.com>
2020-11-02 19:59:56 +0200

    ledcolor: fix led iteration synchronization

--- utils/animated_color.py
+++ utils/animated_color.py
@@ -8,6 +8,25 @@ class AnimatedColor(Color):
         self.__on_duration: float = on_duration
         self.__fade_duration: float = fade_duration
 
+    def __eq__(self, other) -> bool:
+        if not isinstance(other, self.__class__):
+            return False
+
+        if self._rgb != other._rgb:
+            return False
+
+        if self.__on_duration != other.__on_duration:
+            return False
+
+        if self.__fade_duration != other.__fade_duration:
+            return False
+
+        return True
+
+    def __str__(self):
+        return "AnimatedColor -> r: {}, g: {}, b: {}, on: {}, fade: {}" \
+                .format(*self._rgb, self.__on_duration, self.__fade_duration)
+
     @property
     def on_duration(self) -> float:
         return self.__on_duration
